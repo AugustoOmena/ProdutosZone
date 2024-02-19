50ce297b65ccb2ad101e1ae0bda8acce2d23812f
Augusto Omena <augusto.omena.n@gmail.com>
2024-02-19 10:48:43 -0300

    Add EF and Database.

---
+++ ProdutosZone/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace ProdutosZone.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { 
+        }
+    }
+}
