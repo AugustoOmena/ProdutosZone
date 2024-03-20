9618201ec23e00310b6e5908bab1272b376a2356
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-20 19:48:50 -0300

    Creating categorys, associating icons, and updating database.

--- ProdutosZone/Data/ApplicationDbContext.cs
+++ ProdutosZone/Data/ApplicationDbContext.cs
@@ -11,11 +11,31 @@ namespace ProdutosZone.Data
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Device> Device { get; set; }
+        public DbSet<Device> Devices { get; set; }
         public DbSet<ProdutoDevice> ProdutoDevice { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Category>()
+                .HasData(new Category[]
+                {
+                    new Category { Id = 1, Name = "Profissionais de Obras"},
+                    new Category { Id = 2, Name = "Materiais de construção"},
+                    new Category { Id = 3, Name = "Acabamentos finos"},
+                    new Category { Id = 4, Name = "Cursos"},
+                    new Category { Id = 5, Name = "Eletricista"},
+                    new Category { Id = 6, Name = "Encanador"},
+                });
+
+            modelBuilder.Entity<Device>()
+                .HasData(new Models.Device[]
+                {
+                    new Device { Id = 1, Name = "Ouro", Icon = "bi bi-playstation" },
+                    new Device { Id = 2, Name = "Prata", Icon = "bi bi-xbox" },
+                    new Device { Id = 3, Name = "Bronze", Icon = "bi bi-nintendo-switch" },
+                    new Device { Id = 4, Name = "Ruby", Icon = "bi bi-pc-display" }
+                });
+
             modelBuilder.Entity<ProdutoDevice>()
                 .HasKey(e => new { e.ProdutoId, e.DeviceId });
 
