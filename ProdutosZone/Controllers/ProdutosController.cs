4dd58c5accd753b0c419bf19756b9596cd4988a6
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-20 20:41:02 -0300

    Removing privacy zone, creating Products Screen and adding a new favicon image.

---
+++ ProdutosZone/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosZone.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
